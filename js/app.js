e84de285ec7baad744972d2880bdf8f91da69115
eduardorocco <eduardo_rocco@hotmail.it>
2024-11-05 17:00:08 +0100

    REFACTOR for add button and list

--- js/app.js
+++ js/app.js
@@ -4,30 +4,34 @@ function generateEmail() {
 
       .get('https://flynn.boolean.careers/exercises/api/random/mail')
 
-      .then((result) => {
-
-        console.log(result.data.response)
-
-        return result.data.response
-
-      })
-
+      .then(result => result.data.response)
   }
+  
+
+function refreshEmails() {
+
+  const mailList = document.getElementById('mail-list')
+
+  mailList.innerHTML = '' 
 
 
 
+  for (let i = 0; i < 10; i++) {
 
-
-const mailList = document.getElementById('mail-list')
-
-for (let i = 0; i < 10; i++) {
-
-    generateEmail().then((email) => {
+    generateEmail().then(email => {
 
       const listItem = document.createElement('li')
 
-      listItem.textContent = email
+      listItem.textContent = email;
 
-      mailList.appendChild(listItem)
-    });
+      listItem.classList.add('list-group-item')
+
+      mailList.appendChild(listItem);
+
+    })
   }
+}
+
+document.getElementById('refresh-button').addEventListener('click', refreshEmails);
+
+refreshEmails();
